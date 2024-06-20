ff540e41fe397d81ac53b3507b4a4b2a01b067e9
Melissa <lynch.melissat@gmail.com>
2024-06-20 17:26:09 -0400

    mvp for list

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+let ol = document.querySelector('ol');
+
+function addItem() {
+	let li = document.createElement('li');
+	let text = document.createTextNode("New hello");
+	li.appendChild(text);
+	ol.appendChild(li);
+}
+
+function deleteItem() {
+	ol.removeChild(ol.lastElementChild);
+}
